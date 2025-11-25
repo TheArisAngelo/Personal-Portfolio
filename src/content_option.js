6f8389dec88a359e8ae447e0c168d1ec1cd6faa1
TheArisAngelo <aris.angelo.don@outlook.com>
2025-11-25 17:00:56 +0800

    Personal Portfolio Deployment

---
+++ src/content_option.js
@@ -0,0 +1,190 @@
+const logotext = "Don";
+const meta = {
+  title: "Don Florentino",
+  description:
+    "I’m Don Florentino a Front-end Web Developer at Silver Goose 8 International",
+};
+
+const introdata = {
+  title: "I’m Don!",
+  animated: {
+    first: "I love coding",
+    second: "I make cool Websites",
+    third: "I develop Websites",
+  },
+  description:
+    "I'm a Front-end Developer at Silver Goose 8 International. I'm interested in Web Development, it can be Front-end or even Back-end Development.",
+  your_img_url: "https://images.unsplash.com/photo-1546146830-2cca9512c68e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D",
+};
+
+const dataabout = {
+  title: "A little bit about my self",
+  aboutme:
+    "I am a Front-end Developer at Silver Goose 8 International. I'm interested in Web Development, Software Development. I'm currently learning Back-end Development, JS Libraries/FrameWorks, Three,js",
+};
+const worktimeline = [
+  {
+    jobtitle: "Web Developer",
+    where: "Lloyds Financing Corporation",
+    date: "2025",
+  },
+  {
+    jobtitle: "Front-end Web Developer",
+    where: "Silver Goose 8 International",
+    date: "2025",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 80,
+  },
+  {
+    name: "Javascript",
+    value: 50,
+  },
+  {
+    name: "React",
+    value: 50,
+  },
+  {
+    name: "NPM",
+    value: "50"
+  },
+  {
+    name: "PHP",
+    value: "40"
+  },
+  {
+    name: "CodeIgniter",
+    value: "50"
+  },
+  {
+    name: "Photoshop",
+    value: "40"
+  }
+];
+
+const services = [
+  {
+    title: "UI & UX",
+    description:
+      "If you guys want me to help you or collaborate on something, you should E-mail at aris.angelo.don@outlook.com",
+  },
+  {
+    title: "Front-end Projects",
+    description:
+      "As a Front-end Developer, I would glady collaborate on projects that excites me and challenges me.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://private-user-images.githubusercontent.com/143075331/393730878-44b78f42-b682-4e2c-ba90-55b18c7fbd3b.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTg3MDE2NTgsIm5iZiI6MTc1ODcwMTM1OCwicGF0aCI6Ii8xNDMwNzUzMzEvMzkzNzMwODc4LTQ0Yjc4ZjQyLWI2ODItNGUyYy1iYTkwLTU1YjE4YzdmYmQzYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwOTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDkyNFQwODA5MThaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lOTM2ZDQ5Mzc1NWRmZWYzZWZlMjIzNjhiYzRjYzUxMzQ5NDdkNDNlNjdmMDYyYjVkYWQzMjE1MTVlNzM1ZjgzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.oYb82ZJE8hSZWEufqae4lr7FR-eJwjDi_0SAZ5Eh6CM",
+    description:
+      "My passion for making Cool Websites, produced this. A Car (RWB) Website.",
+    link: "https://car-website-pearl-seven.vercel.app/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "aris.angelo.don@outlook.com",
+  YOUR_FONE: "09563740075",
+  description:
+    "If you guys want to have a joint project or have a question about Developing, just let me know. Email & Phone number is provided. Take care as always. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/TheArisAngelo",
+  facebook: "https://web.facebook.com/TheArisAngelo/",
+  linkedin: "https://www.linkedin.com/in/aris-angelo-don-florentino-100599313", 
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
